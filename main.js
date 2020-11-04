192107e855cbc69db8cecb03e6ad3db5b5b5b613
Ryan Proctor <49620154+rproct@users.noreply.github.com>
2020-11-04 11:37:24 -0800

    Finished Landing Page

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
 
 //Landing Page
 app.get('/', function(req, res){
