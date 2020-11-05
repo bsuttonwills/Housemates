36bb5a2c64a356287168b09770b92e48f22d5541
nathanlatronica <nlatronica@csumb.edu>
2020-11-04 20:34:59 -0800

    group / add member page

--- main.js
+++ main.js
@@ -1,12 +1,29 @@
 var express = require('express');
 var app = express();
 
+const port = process.env.PORT || 8000;
+
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+
 
 //Landing Page
 app.get('/', function(req, res){
     res.render('pages/home');
 });
 
-app.listen(3000);
+app.get('/group', function(req, res){
+    res.render('pages/createGroup');
+});
+
+app.get('/addMember', function(req, res){
+    res.render('pages/AddMember');
+});
+
+
+
+//starting server
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`)
+  });
+ 
